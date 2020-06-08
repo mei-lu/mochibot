3e75bbbb69167c29b0c7bfaddfe08693ed35cb80
Mei <meiluwork@gmail.com>
2020-06-07 21:47:36 -0400

    catch block for async

--- index.js
+++ index.js
@@ -13,14 +13,19 @@ client.on('message', async message => {
      let command = message.content.replace('~', '')
      console.log(command)
     if (message.content.startsWith('~')) {
-        await fetch(`https://mochimoji-api.herokuapp.com/kaomojis/${command}`).then(response => response.json())
-        .then(data => {
-            if (data === undefined) {
-                message.channel.send('Invalid emotion (╥_╥)')
-            } else {
-                message.channel.send(data[0].kaomoji)
-            }
-        })
+        try{
+            await fetch(`https://mochimoji-api.herokuapp.com/kaomojis/${command}`).then(response => response.json())
+            .then(data => {
+                if (data === undefined) {
+                    message.channel.send('Invalid emotion (╥_╥)')
+                } else {
+                    message.channel.send(data[0].kaomoji)
+                }
+            })
+        } catch (err) {
+            message.channel.send('No kaomoji (╥_╥)')
+        }
+        
         
     }
 })
