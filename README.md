320ba995d0d83c64cbabda8c9f5583121e88be22
Mei <meiluwork@gmail.com>
2020-06-07 21:37:52 -0400

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# MochiBot
+
+MochiBot is a discord bot to help you send cute kaomojis (emoticons derived from special characters) in your Discord channel.
+Currently, 4 emotions are supported: happy, sad, angry, and love!
+
+## Commands
+
+Happy
+
+```happy
+~happy
+```
+
+Sad
+
+```sad
+~sad
+```
+
+Angry
+
+```angry
+~angry
+```
+
+Love
+
+```love
+~love
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const Discord = require('discord.js')
+const client = new Discord.Client();
+const fetch = require('node-fetch')
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.BOT_TOKEN)
+
+client.on('message', async message => {
+     let command = message.content.replace('~', '')
+     console.log(command)
+    if (message.content.startsWith('~')) {
+        await fetch(`https://mochimoji-api.herokuapp.com/kaomojis/${command}`).then(response => response.json())
+        .then(data => {
+            if (data === undefined) {
+                message.channel.send('Invalid emotion (╥_╥)')
+            } else {
+                message.channel.send(data[0].kaomoji)
+            }
+        })
+        
+    }
+})
